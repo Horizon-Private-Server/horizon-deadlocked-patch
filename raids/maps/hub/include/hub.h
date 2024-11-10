24bd7b49a176478bf3924567a6ae7f5a765f8993
Daniel Gerendasy <danielgerendasy@gmail.com>
2024-11-09 17:15:19 -0800

    Remove raids custom maps code

--- raids/maps/hub/include/hub.h
+++
@@ -1,6 +0,0 @@
-#ifndef _RAIDS_HUB_H_
-#define _RAIDS_HUB_H_
-
-#define MAP_BASE_COMPLEXITY         (5000)
-
-#endif // _RAIDS_HUB_H_
--- raids/maps/sarathos/include/sarathos.h
+++
@@ -1,6 +0,0 @@
-#ifndef _RAIDS_SARATHOS_H_
-#define _RAIDS_SARATHOS_H_
-
-#define MAP_BASE_COMPLEXITY         (5000)
-
-#endif // _RAIDS_SARATHOS_H_
