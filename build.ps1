68d878ab1a4f12296a3be065a0d8cc0aee655388
Daniel Gerendasy <Dnawrkshp@users.noreply.github.com>
2025-10-27 15:59:15 -0700

    Add build scripts

---
+++ build.ps1
@@ -0,0 +1,7 @@
+# remove current and create new bin directory for horizon-deadlocked-patch/
+Remove-Item -Recurse -Force ./bin
+mkdir ./bin
+
+# run docker and run "docker-init.sh"
+docker run -v ${PWD}:/src ps2dev/ps2dev:v1.2.0 /bin/sh -c "cd src; sh docker-init.sh"
+
---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Check if bin directory exists, if not create it, otherwise clear its contents
+if [ -d bin ]; then
+  # Directory exists, so delete all files inside it
+  rm -rf bin/*
+else
+  # Directory doesn't exist, so create it
+  mkdir bin
+fi
+
+# run docker and run "docker-init.sh"
+docker run -v "${PWD}":/src ps2dev/ps2dev:v1.2.0 /bin/sh -c "cd src; sh docker-init.sh"
