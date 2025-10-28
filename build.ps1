27c9a842d8e389140cde2285f566e9d589b0b4a8
Daniel Gerendasy <danielgerendasy@gmail.com>
2025-10-27 21:43:50 -0700

    Add make commands to build scripts

--- build.ps1
+++ build.ps1
@@ -3,5 +3,5 @@ Remove-Item -Recurse -Force ./bin
 mkdir ./bin
 
 # run docker and run "docker-init.sh"
-docker run -v ${PWD}:/src ps2dev/ps2dev:v1.2.0 /bin/sh -c "cd src; sh docker-init.sh"
+docker run -v ${PWD}:/src ps2dev/ps2dev:v1.2.0 /bin/sh -c "cd src; sh docker-init.sh; make clean; make"
 
--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ else
 fi
 
 # run docker and run "docker-init.sh"
-docker run -v "${PWD}":/src ps2dev/ps2dev:v1.2.0 /bin/sh -c "cd src; sh docker-init.sh"
+docker run -v "${PWD}":/src ps2dev/ps2dev:v1.2.0 /bin/sh -c "cd src; sh docker-init.sh; make clean; make"
