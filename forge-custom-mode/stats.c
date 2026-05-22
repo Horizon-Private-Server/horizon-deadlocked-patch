23b191d094c47b344d863d9f347f42cf6c10b6ae
Daniel Gerendasy <danielgerendasy@gmail.com>
2026-05-22 16:48:10 -0700

    feat(forgecgm): add CGM_SCORE_STAT_TYPE_TIME_MILLISECONDS stat type

--- forge-custom-mode/stats.c
+++ forge-custom-mode/stats.c
@@ -13,14 +13,26 @@ void statsFormatValue(char* dest, int size, int value, int format)
   switch (format)
   {
     case CGM_SCORE_STAT_TYPE_INT:
-      uiPrintCommaNumber(dest, size, value, 0);
-      return;
+      {
+        uiPrintCommaNumber(dest, size, value, 0);
+        return;
+      }
     case CGM_SCORE_STAT_TYPE_TIME_SECONDS:
-      snprintf(dest, size, "%02d:%02d", value / 60, value % 60);
-      return;
+      {
+        snprintf(dest, size, "%02d:%02d", value / 60, value % 60);
+        return;
+      }
+    case CGM_SCORE_STAT_TYPE_TIME_MILLISECONDS:
+      {
+        int sec = value / 1000;
+        snprintf(dest, size, "%02d:%02d", sec / 60, sec % 60);
+        return;
+      }
     case CGM_SCORE_STAT_TYPE_FLOAT:
-      snprintf(dest, size, "%.2f", value / 1024.0);
-      return;
+      {
+        snprintf(dest, size, "%.2f", value / 1024.0);
+        return;
+      }
   }
 }
 
